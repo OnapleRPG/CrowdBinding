5f91fa4d5becf8cc24eeb33c681180f9443148f9
Raiponz <spooki.leserpent@orange.fr>
2019-05-01 17:26:03 +0200

    update interface

--- src/main/java/com/onaple/crowdbinding/service/GroupService.java
+++ src/main/java/com/onaple/crowdbinding/service/GroupService.java
@@ -7,9 +7,9 @@ import java.util.Optional;
 import java.util.UUID;
 
 public interface GroupService {
-    Optional<UUID> getGroupLeader(UUID groupId);
+    Optional<Player> getGroupLeader(UUID groupId);
 
     Optional<UUID> getGroupId(Player player);
 
-    List<UUID> getMembers(UUID groupId);
+    List<Player> getMembers(UUID groupId);
 }
--- src/main/java/com/onaple/crowdbinding/service/SimpleGroupService.java
+++ src/main/java/com/onaple/crowdbinding/service/SimpleGroupService.java
@@ -1,21 +1,14 @@
 package com.onaple.crowdbinding.service;
 
 import com.onaple.crowdbinding.CrowdBinding;
-import com.onaple.crowdbinding.GroupManager;
 import com.onaple.crowdbinding.data.Group;
-import com.onaple.crowdbinding.exceptions.UnknownGroupException;
-import org.spongepowered.api.Server;
-import org.spongepowered.api.Sponge;
 import org.spongepowered.api.entity.living.player.Player;
 
-import javax.inject.Inject;
 import javax.inject.Singleton;
-import javax.swing.text.html.Option;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
-import java.util.stream.Collectors;
 
 @Singleton
 public class SimpleGroupService implements GroupService {
@@ -23,7 +16,7 @@ public class SimpleGroupService implements GroupService {
     public SimpleGroupService() {}
 
     @Override
-    public Optional<UUID> getGroupLeader(UUID groupId){
+    public Optional<Player> getGroupLeader(UUID groupId){
         Optional<Group> groupOptional = CrowdBinding.getGroupManager().getGroup(groupId);
         return groupOptional.map(Group::getLeader);
     }
@@ -35,7 +28,7 @@ public class SimpleGroupService implements GroupService {
     }
 
     @Override
-    public List<UUID> getMembers(UUID groupId){
+    public List<Player> getMembers(UUID groupId){
         Optional<Group> groupOptional = CrowdBinding.getGroupManager().getGroup(groupId);
         return groupOptional.map(group -> new ArrayList<>(group.getPlayers())).orElseGet(ArrayList::new);
     }
